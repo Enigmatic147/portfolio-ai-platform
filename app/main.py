6c73962b3f2df5ed30b193dffa4629eab2bfc8bd
Pranay Mishra <hardrock147@gmail.com>
2026-06-04 17:14:02 +0530

    Add CORS support

--- app/main.py
+++ app/main.py
@@ -2,11 +2,17 @@ from fastapi import FastAPI
 
 from app.routes.projects import router as projects_router
 from app.routes.blogs import router as blogs_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="AI Portfolio Platform"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(
     projects_router,
     prefix="/projects",
